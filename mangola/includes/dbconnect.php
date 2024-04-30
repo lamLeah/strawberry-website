7fafeb1a29f491dcfb5c834d2d9ccb3c3b350465
lamLeah <mc35094@umac.mo>
2024-04-30 15:18:14 +0800

    initial

---
+++ mangola/includes/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+//connect to the DB
+//connect to the server(server_name,server_user_id,server_user_password)
+$connection = mysqli_connect("127.0.0.1", "pmauser", "danjkl");
+
+//connect to the database(access_info_variable,database_name)
+mysqli_select_db($connection, "mangola");
