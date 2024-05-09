e41aac6e92d4869b1cbb20429a62f6e134077cec
lamLeah <mc35094@umac.mo>
2024-05-10 02:28:14 +0800

    services

--- mangola/products.php
+++ mangola/products.php
@@ -16,6 +16,7 @@ if (($_SESSION['email'] == "admin@strawberryHeaven.com")) {
   include "includes/header_postlogin.php";
 }
 ?>
+<link rel="stylesheet" type="text/css" href="./css/aboutUsSection.css">
 
 <body style="background-image: url('images/background2.jpg')!important;">
 
@@ -46,29 +47,81 @@ if (($_SESSION['email'] == "admin@strawberryHeaven.com")) {
     <div class="row">
 
       <!--Bio-Section in 10/12 parts-->
+
+
       <div class="col-md-10">
-        <div class="row">
-
-          <div class="col-md-12 bio-tab">
-            <div class="row">
-              <div class="col-md-4">
-                <img src="images/website_logo.jpeg" class="img-size img-circle" style="object-fit: none;">
+        <div class="col-md-12" style="padding: 0; margin:12px 0">
+          <img src="images/wallpaper1.jpg" class="img-size-lg">
+        </div>
+        <div class="row ">
+          <section class="container">
+            <div class="tab-container">
+              <div class="tabs">
+                <div id="tab1" class="tab active">About us</div>
+                <div id="tab2" class="tab">Return or Exchange</div>
+                <div id="tab3" class="tab">Express</div>
               </div>
+              <div class="tab-content">
+                <div data-tab="tab1" style="display: block">
+                  <div class="col-md-12 bio-tab">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <img src="images/website_logo.jpeg" class="img-size img-circle" style="object-fit: none;">
+                      </div>
 
-              <div class="col-md-8">
-                <h1 class="text-center"> <b>About StrawberryHeaven.com</b> </h1>
-                <p>&nbsp&nbsp&nbsp&nbsp<b><i>Online Strawberry Shopping Site <b>Strawberry Heaven</b>'s vision is to create the world's most reliable, frictionless commerce ecosystem, creating life-changing experiences for buyers and sellers.StrawberryHeaven.com is the world's largest online strawberry shopping marketplace. An online shopping trend is becoming a household name and so is <b>Strawberry Heaven</b>.
-                    </i></b></p>
+                      <div class="col-md-8">
+                        <h1 class="text-center"> <b>About StrawberryHeaven.com</b> </h1>
+                        <p>&nbsp&nbsp&nbsp&nbsp<b><i>Online Strawberry Shopping Site <b>Strawberry Heaven</b>'s vision is to create the world's most reliable, frictionless commerce ecosystem, creating life-changing experiences for buyers and sellers.StrawberryHeaven.com is the world's largest online strawberry shopping marketplace. An online shopping trend is becoming a household name and so is <b>Strawberry Heaven</b>.
+                            </i></b></p>
+                      </div>
+                    </div>
+                  </div>
+
+
+                </div>
+                <div data-tab="tab2" style="display: none">
+                  <h2>Return or Exchange Policy:</h2>
+                  <p>
+                  <ol>
+                    <li><b>Eligibility: </b>We accept returns or exchanges within 7 days of the delivery date. To be eligible, the strawberries must be in their original packaging and in the same condition as when they were received. Please note that perishable items, such as fresh strawberries, cannot be returned or exchanged if they have been consumed or mishandled.</li>
+                    <li><b>Initiating a Return or Exchange: </b>If you wish to initiate a return or exchange, please contact our customer support team within the specified timeframe. Provide your order details and the reason for the return or exchange. Our team will guide you through the process and provide you with a return authorization.</li>
+                    <li><b>Return Shipping: </b>When returning strawberries, please ensure they are securely packaged to prevent damage during transit. We recommend using a reliable shipping service with tracking capabilities. The cost of return shipping will be your responsibility, unless the return is due to an error on our part.</li>
+                    <li><b>Refunds or Exchanges: </b>Once we receive the returned strawberries and verify their condition, we will process your refund or exchange. If you opted for a refund, it will be issued using the original payment method within a specified timeframe. In the case of an exchange, we will ship the replacement strawberries to you as soon as possible.</li>
+                    <li><b>Quality Concerns: </b>If you believe the strawberries you received are of subpar quality or not as described, please provide detailed information and supporting evidence. We take such concerns seriously and will investigate the issue promptly. Depending on the circumstances, we may offer a refund, exchange, or store credit.</li>
+
+                  </ol>
+                  </p>
+                </div>
+                <div data-tab="tab3" style="display: none">
+                  <h2>Express Policy:</h2>
+                  <p>
+                  <ol>
+                    <li><b>Express Delivery: </b>We understand that you may have a craving for delicious strawberries that cannot wait. That's why we offer express delivery services for selected areas. If you are in an eligible location, you can choose the express delivery option during the checkout process to receive your strawberries within a shorter timeframe.
+                    </li>
+                    <li><b>Cut-Off Times: </b>To avail of express delivery, please ensure you place your order before the specified cut-off time. Orders received after the cut-off time will be processed and shipped on the following business day. The exact cut-off time will be clearly communicated on our website and during the checkout process.
+                    </li>
+                    <li><b>Delivery Charges: </b>Express deliveries may be subject to additional delivery charges due to the expedited nature of the service. The exact charges will be displayed during the checkout process, allowing you to review and confirm before completing your order.
+                    </li>
+                    <li><b>Tracking and Notifications: </b>For express deliveries, we provide detailed tracking information so you can monitor the progress of your order. You will receive notifications via email or SMS regarding the status of your delivery, including estimated arrival times and any updates or delays that may occur.
+                    </li>
+                    <li><b>Availability and Coverage: </b>Please note that express delivery services may not be available in all areas. We strive to expand our coverage, but there may be certain locations that are not eligible for express delivery due to logistical constraints. During the checkout process, you will be informed if express delivery is available for your address.
+                    </li>
+
+                  </ol>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
 
-          <div class="col-md-12">
-            <img src="images/wallpaper1.jpg" class="img-size-lg">
-          </div>
+
+
 
         </div>
+
       </div>
+
+
 
       <!--List of items in 2/12 parts-->
       <div class="col-md-2">
@@ -103,6 +156,7 @@ if (($_SESSION['email'] == "admin@strawberryHeaven.com")) {
     <?php include "includes/footer.php"; ?>
 
   </div>
+  <script src="./js/aboutUsChangeTab.js"></script>
 </body>
 
 </html>
