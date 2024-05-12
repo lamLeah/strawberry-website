d94591503ae9dd6720003c9c66de7069df511018
LevanaLeong <mc35107@um.edu.mo>
2024-05-12 16:33:48 +0800

    modify product_discount

--- mangola/includes/dbconnect.php
+++ mangola/includes/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 //connect to the DB
 //connect to the server(server_name,server_user_id,server_user_password)
-$connection = mysqli_connect("127.0.0.1", "pmauser", "danjkl");
+$connection = mysqli_connect("127.0.0.1", "root", "");
 
 //connect to the database(access_info_variable,database_name)
 mysqli_select_db($connection, "mangola");
