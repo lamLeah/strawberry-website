f4ed97369725f3583bff985f22af86db1acba0c4
lamLeah <mc35094@umac.mo>
2024-05-12 22:13:50 +0800

    login with hash code

--- mangola/login_user.php
+++ mangola/login_user.php
@@ -14,7 +14,8 @@ $password = $_POST['user_password'];
 
 // $pwd_decrpt = password_verify($password, $hash)
 
-$query = "SELECT * FROM `users` WHERE `email` LIKE '$email' AND `password` LIKE '$password'";
+// $query = "SELECT * FROM `users` WHERE `email` LIKE '$email' AND `password` LIKE '$password'";
+$query = "SELECT * FROM `users` WHERE `email` LIKE '$email'";
 
 //running the serch in DB and storing in $result
 $result = mysqli_query($connection, $query);
@@ -25,19 +26,23 @@ $num_rows = mysqli_num_rows($result);
 
 if ($num_rows == 1) {
 	//correct login
-
 	//retriving session name
-
 	$row = mysqli_fetch_assoc($result);
-	$_SESSION['name'] = $row['name'];
-	$_SESSION['email'] = $row['email'];
-	$_SESSION['user_id'] = $row['user_id'];
 
 
 	if (($_SESSION['email'] == "admin@strawberryHeaven.com") && ($row['password'] == "1234")) {
 		header('Location: admin.php');
-	} else
-		header('Location: products.php');
+	} else {
+		// check pwd
+		if (password_verify($password, $row['password'])) {
+			$_SESSION['name'] = $row['name'];
+			$_SESSION['email'] = $row['email'];
+			$_SESSION['user_id'] = $row['user_id'];
+			header('Location: products.php');
+		} else {
+			header('Location: index.php');
+		}
+	}
 } else {	//incorrect login
 	//redirect
 	header('Location: index.php');
